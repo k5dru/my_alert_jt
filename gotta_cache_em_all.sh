11c4bf5bc102e2e31d78c7f782ebc51b6bedd6a9
k5dru <james.lemley@gmail.com>
2016-06-04 19:27:54 -0500

    Add files via upload
    Changed distance calculation to be more correct and re-ran job to pre-cache every grid in the world

--- gotta_cache_em_all.sh
+++ gotta_cache_em_all.sh
@@ -1,5 +1,13 @@
+./check_log_for_grid ~/.local/share/WSJT-X/wsjtx.log EM35
+
+cat ~/.local/share/WSJT-X/ALL.TXT ~/.local/share/WSJT-X/ALL_WSPR.TXT | 
+tr ' ' '\n' | grep '^[A-Z][A-Z][0-9][0-9]$' | sort -u | 
+while read thing; do 
+  ./check_log_for_grid /dev/null $thing
+done
+
 for i in A B C D E F G H I J K L M N O P Q R ; do 
-for j in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z; do 
+for j in A B C D E F G H I J K L M N O P Q R ; do 
 for k in 0 1 2 3 4 5 6 7 8 9; do 
 for l in 0 1 2 3 4 5 6 7 8 9; do 
   ./check_log_for_grid /dev/null $i$j$k$l
